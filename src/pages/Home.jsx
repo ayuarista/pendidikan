57cb722a29b205734a043390af478d6c7d6c475a
ayuarista <aristaadewi8@gmail.com>
2026-03-08 20:56:26 +0800

    create header and structure components

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div className="relative w-full min-h-screen bg-cover bg-center flex justify-center items-center text-white bg-blend-multiply bg-black/30" style={{ backgroundImage: `url(${'https://i.pinimg.com/736x/b5/57/0e/b5570e8488686a615e76a2501c098cd1.jpg'})` }}>
+            <h1 className="text-4xl font-bold">INI CUMA CONTOH GAMBAR Y</h1>
+
+        </div>
+    )
+}
+
+export default Home
